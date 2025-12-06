86d9b8737f264b5224fa7131eeb966dacb094ede
Vishwajeet Vishwakarma <jeet31054@gmail.com>
2025-12-06 13:04:34 +0530

    2."Adding Hello World").

---
+++ src/Hello.java
@@ -0,0 +1,6 @@
+public class Hello {
+    static void main() {
+        System.out.println("hello world");
+        System.out.println("Hey! i am vishwajeet vishwakama and i am happy to learn java.");
+    }
+}
