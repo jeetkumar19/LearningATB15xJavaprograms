0f686f79094eb0e35ae8d2c32bb12823ad47a68c
Vishwajeet Vishwakarma <jeet31054@gmail.com>
2025-12-06 13:50:40 +0530

    4.Added Calculation program , simple math

---
+++ src/Calculation.java
@@ -0,0 +1,9 @@
+public class Calculation {
+   public static void main(String[] args) {
+       System.out.println(10 + 200);
+       System.out.println(1000 / 50);
+       System.out.println(234 * 2344);
+       System.out.println(40 - 10);
+
+    }
+}
