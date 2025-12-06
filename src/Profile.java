caa43ef3b2eb3d242ca9ddbe93748ea685dbd367
Vishwajeet Vishwakarma <jeet31054@gmail.com>
2025-12-06 13:41:16 +0530

    3.Added Profile program with full main method

---
+++ src/Profile.java
@@ -0,0 +1,8 @@
+public class Profile {
+    public static void main(String[] args) {
+        System.out.println("Name: vishwajeet vishwakarma");
+        System.out.println("Age:26");
+        System.out.println("City:Hyderabad");
+        System.out.println("Dream Job: Automation Tester");
+    }
+}
