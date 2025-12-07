507af3bbaaed6d8ec5c64154cc7836e39d182269
Vishwajeet Vishwakarma <jeet31054@gmail.com>
2025-12-07 12:14:56 +0530

    6.feat: add Google ID card program using Java variables.

---
+++ src/GooleID.java
@@ -0,0 +1,35 @@
+import java.sql.SQLOutput;
+
+public class GooleID {
+    public static void main(String[] args) {
+        String name = "vishwajeet vishwakarma";  // name of the employee
+        System.out.println(name);
+
+        String designation = "Softwere engineeer";
+        System.out.println(designation);
+        int    employee_ID= 435323;
+        System.out.println(employee_ID);
+        double salary= 1200000.234;
+        System.out.println(salary);
+
+
+        System.out.println("--------------------------");
+        System.out.println("   GOOGLE ID CARD         ");
+        System.out.println("--------------------------");
+
+
+        System.out.println("Name:" + name );
+        System.out.println("Role: " + designation);
+        System.out.println("ID: " + employee_ID);
+        System.out.println("Pay: " + salary);
+
+
+
+    }
+
+
+
+
+}
+
+
