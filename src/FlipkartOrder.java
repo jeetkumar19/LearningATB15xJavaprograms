33309496f4350e291fffb1547f52908a7a6acbfa
Vishwajeet Vishwakarma <jeet31054@gmail.com>
2025-12-07 12:40:57 +0530

    7.Add dummy recepit of FlipkartOrder

---
+++ src/FlipkartOrder.java
@@ -0,0 +1,24 @@
+public class FlipkartOrder {
+    public static void main(String[] args) {
+        String item ="bluetooh speaker";
+
+        double price=1342.45;
+
+        int   quantity=4;
+
+        // total cost of the item
+        double total_cost= price * quantity;
+        System.out.println(total_cost);
+        System.out.println("----------------------");
+        System.out.println("FLIPKART RECEIPT");
+        System.out.println("______________________");
+        System.out.println("Item:   " + item);
+        System.out.println("Price:  " + price);
+        System.out.println("Qty:    " + quantity);
+        System.out.println("-------------------------");
+        System.out.println("Total:  " + total_cost);
+        System.out.println("-------------------------");
+
+
+    }
+}
