70765f0255b809233ef3837007bc1583e9e77856
Vishwajeet Vishwakarma <jeet31054@gmail.com>
2025-12-08 14:06:44 +0530

    8.Adding my personal details

---
+++ src/Userprofile.java
@@ -0,0 +1,25 @@
+import java.sql.SQLOutput;
+
+public class Userprofile {
+   public static void main(String[] args) {
+       String name="vishwajeet vishwakarma";
+       int age=26;
+       char gender='m';
+       String nationality="indian";
+       String blood_group="b+";
+
+       System.out.println("--------------------------");
+       System.out.println("-----OFFICIAL ID CARD-----");
+       System.out.println("--------------------------");
+
+       System.out.println("Name:"  + name);
+       System.out.println("Age:" + age   );
+       System.out.println("Gender: "+ gender);
+       System.out.println("Nationality:" + nationality);
+       System.out.println("Blood_group:" +blood_group);
+       System.out.println("*****************************");
+
+    }
+
+
+}
