0aa50466767a3ae108b9a7218ca854d88f7ee1f2
Vishwajeet Vishwakarma <jeet31054@gmail.com>
2025-12-15 17:46:36 +0530

    adding keyword identifier

---
+++ src/ex_02_java_Basics/Lab005_Keyword_identifier.java
@@ -0,0 +1,12 @@
+package ex_02_java_Basics;
+
+public class Lab005_Keyword_identifier {
+    static void main() {
+        System.out.println("keywords!");
+
+        // keywords in lower case and they have special meaning.
+
+        // package , public , static , void
+    }
+
+}
