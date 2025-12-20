79c01ce98019cde98530f919564fc959af7ebf4e
Vishwajeet Vishwakarma <jeet31054@gmail.com>
2025-12-20 18:00:11 +0530

    adding numeric data values

---
+++ src/ex_02_java_Basics/Lab_10_numeric_data_types.java
@@ -0,0 +1,26 @@
+package ex_02_java_Basics;
+
+public class Lab_10_numeric_data_types {
+    static void main() {
+        int x = 123;
+        byte b = 10;
+        float f =10.34f;
+        short s =10;
+        char c1='@';
+        char c2='#';
+        char c3='%';
+        long phone=9123436106l;
+        double d=2.3454636;
+        System.out.println(x);
+        System.out.println(d);
+        System.out.println(c3);
+        System.out.println(c2);
+        System.out.println(c1);
+        System.out.println(phone);
+        System.out.println(b);
+        System.out.println(f);
+        System.out.println(s);
+
+
+    }
+}
