fa0446f14f5d3b339a31a1e717191b1e35d9bea7
Vishwajeet Vishwakarma <jeet31054@gmail.com>
2025-12-20 18:00:51 +0530

    adding print f function

---
+++ src/ex_02_java_Basics/Lab0011_Printf.java
@@ -0,0 +1,13 @@
+package ex_02_java_Basics;
+
+public class Lab0011_Printf {
+
+    static void main() {
+        System.out.println("jeet kumar"); // if you use println , it will add a new line.
+        System.out.print("age = 123");    // if you use print only , i will not add a new line.
+        System.out.printf(" this is a normal text ");
+
+        int a=10;
+        System.out.printf("value of a is %d",+a);
+    }
+}
