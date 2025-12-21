905fba9974995343ddf4f8a3fc5ed9975db21acf
Vishwajeet Vishwakarma <jeet31054@gmail.com>
2025-12-21 13:00:01 +0530

    adding constants

---
+++ src/ex_03_variable_data_typess/Lab0013_constants.java
@@ -0,0 +1,19 @@
+package ex_03_variable_data_typess;
+
+public class Lab0013_constants {
+    public static void main(String[] args) {
+        int a=12;// it not a constant variable because can be chandged anytime.
+
+        a=13;     //
+        System.out.println(a);
+
+
+        final float PI=3.14f;  // this value can't be changed now.
+        System.out.println(PI);
+        //float _pi=3.1234f;
+
+
+
+
+    }
+}
