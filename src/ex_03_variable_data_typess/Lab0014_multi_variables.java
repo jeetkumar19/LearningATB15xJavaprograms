a6856122c327918ebd141f85eb22f0a227ee91e7
Vishwajeet Vishwakarma <jeet31054@gmail.com>
2025-12-21 13:00:57 +0530

    adding multipile_variables

---
+++ src/ex_03_variable_data_typess/Lab0014_multi_variables.java
@@ -0,0 +1,12 @@
+package ex_03_variable_data_typess;
+
+public class Lab0014_multi_variables {
+    static void main() {
+        int a=10 , b=23 ,c=123;
+        int $=1453;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println($);
+    }
+}
