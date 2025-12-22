977b802a68c9386cafd794dc01a4f1ece2000208
Vishwajeet Vishwakarma <jeet31054@gmail.com>
2025-12-22 13:32:50 +0530

    adding personal profile.

---
+++ src/ex_03_variable_data_typess/Lab0024_even_odd.java
@@ -0,0 +1,20 @@
+package ex_03_variable_data_typess;
+
+public class Lab0024_even_odd {
+
+
+
+        public static void main(String[] args) {
+            int age = 22;
+            double salary = 45000.50;
+            char grade = 'A';
+            boolean isEmployed = true;
+
+            System.out.println("Age: " + age);
+            System.out.println("Salary: " + salary);
+            System.out.println("Grade: " + grade);
+            System.out.println("Employed: " + isEmployed);
+
+    }
+
+}
