bfd79bc01bab841cdd51a123e42c93423de60130
Vishwajeet Vishwakarma <jeet31054@gmail.com>
2025-12-23 13:55:52 +0530

    adding mini challenge as practice

---
+++ src/ex_03_variable_data_typess/Lab0025_mini_challenge.java
@@ -0,0 +1,28 @@
+package ex_03_variable_data_typess;
+
+public class Lab0025_mini_challenge {
+    public static void main(String[] args) {
+
+        byte b = 100;
+        short s = 32000;
+        int i = 100000;
+        long l = 10000000000L;
+
+        float f = 10.5f;
+        double d = 99.99;
+
+        char c = 'J';
+        boolean isJavaFun = true;
+
+        System.out.println("Byte: " + b);
+        System.out.println("Short: " + s);
+        System.out.println("Int: " + i);
+        System.out.println("Long: " + l);
+        System.out.println("Float: " + f);
+        System.out.println("Double: " + d);
+        System.out.println("Char: " + c);
+        System.out.println("Boolean: " + isJavaFun);
+    }
+}
+
+
