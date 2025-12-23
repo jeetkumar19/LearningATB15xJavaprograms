63fa934614634dd0e1a857e91fab242598702e19
Vishwajeet Vishwakarma <jeet31054@gmail.com>
2025-12-23 13:59:17 +0530

    adding mini challenge as practice of litrals

---
+++ src/ex_04_literals/Lab0026_adding_mini_challenge_literals.java
@@ -0,0 +1,29 @@
+package ex_04_literals;
+
+public class Lab0026_adding_mini_challenge_literals {
+
+        public static void main(String[] args) {
+
+            int decimal = 10;
+            int binary = 0b1010;
+            int octal = 012;
+            int hex = 0xA;
+
+            float floatLiteral = 10.5f;
+            char charLiteral = 'A';
+            String stringLiteral = "Java";
+            boolean boolLiteral = true;
+
+            System.out.println("Decimal: " + decimal);
+            System.out.println("Binary: " + binary);
+            System.out.println("Octal: " + octal);
+            System.out.println("Hexadecimal: " + hex);
+
+            System.out.println("Float: " + floatLiteral);
+            System.out.println("Char: " + charLiteral);
+            System.out.println("String: " + stringLiteral);
+            System.out.println("Boolean: " + boolLiteral);
+        }
+    }
+
+
