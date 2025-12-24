f9b6cdd4c6b27fb2885e80e4357847be6e892a1e
Vishwajeet Vishwakarma <jeet31054@gmail.com>
2025-12-24 13:18:12 +0530

    adding all operators and new operators

---
+++ src/ex_04_operators/Lab0035_all_operators.java
@@ -0,0 +1,19 @@
+package ex_04_operators;
+
+public class Lab0035_all_operators {
+    static void main() {
+        boolean b1=true;
+        int a=10;
+        int b=20;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a>b);
+        System.out.println(a<=b);
+        System.out.println(a!=b);
+        System.out.println(a==b);
+
+
+    }
+}
---
+++ src/ex_04_operators/Lab0036_new_operators.java
@@ -0,0 +1,8 @@
+package ex_04_operators;
+
+public class Lab0036_new_operators {
+    static void main() {
+        String name=new String("jeet kumar");
+        // this will learn in opps concept.
+    }
+}
