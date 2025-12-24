39f8c7e66ad8d1553bf9d5f99e90f594f53b870e
Vishwajeet Vishwakarma <jeet31054@gmail.com>
2025-12-24 13:16:14 +0530

    adding some interview questions

---
+++ src/ex_04_operators/Lab0032_interview.java
@@ -0,0 +1,10 @@
+package ex_04_operators;
+
+public class Lab0032_interview {
+    static void main() {
+        System.out.println();
+
+        // === is nothing in java
+        System.out.println(!(20<30));
+    }
+}
