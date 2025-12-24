6f76cdb28ee89d2799febebd1866e7241437a136
Vishwajeet Vishwakarma <jeet31054@gmail.com>
2025-12-24 13:17:19 +0530

    adding OR ans AND gate interview questions.

---
+++ src/ex_04_operators/Lab0033_OR_AND_GATE.java
@@ -0,0 +1,17 @@
+package ex_04_operators;
+
+public class Lab0033_OR_AND_GATE {
+    static void main() {
+        // OR GATE
+        System.out.println(true || true);
+        System.out.println(false || false);
+        System.out.println(true || false);
+        System.out.println(false || true);
+
+        // AND GATE
+        System.out.println(true && true);
+        System.out.println(false && false);
+        System.out.println(true && false);
+        System.out.println(false && true);
+    }
+}
---
+++ src/ex_04_operators/Lab0034_interview.java
@@ -0,0 +1,10 @@
+package ex_04_operators;
+
+public class Lab0034_interview {
+    static void main() {
+        int jeet_salary=12000;
+        boolean b=!(jeet_salary>12000|| jeet_salary<10000);
+        System.out.println(b);
+
+    }
+}
