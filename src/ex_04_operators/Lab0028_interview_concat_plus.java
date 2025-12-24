5bb66705fadde2d79e50908500127149a718b48f
Vishwajeet Vishwakarma <jeet31054@gmail.com>
2025-12-24 13:13:47 +0530

    adding interview question of concatination

---
+++ src/ex_04_operators/Lab0028_interview_concat_plus.java
@@ -0,0 +1,20 @@
+package ex_04_operators;
+
+public class Lab0028_interview_concat_plus {
+    static void main() {
+        // +
+        int a=10;
+        int b=100;
+        System.out.println(a+b);
+
+        // string concatenation
+        String A="jeet";
+        String _b="kumar";
+        System.out.println(A+_b);
+
+         // these arthematic operators is not allowed i string.
+        //System.out.println(A-_b);
+        //System.out.println(A*_b);
+        //System.out.println(A/_b);
+    }
+}
