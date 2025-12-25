0442883df29067ffe3a4506b9aefbc700252f706
Vishwajeet Vishwakarma <jeet31054@gmail.com>
2025-12-25 17:28:32 +0530

    adding execrise identity literals.

---
+++ src/Java_exericise/ex_004_identity_literals.java
@@ -0,0 +1,16 @@
+package Java_exericise;
+
+public class ex_004_identity_literals {
+    static void main(String[] args) {
+        int a = 101;
+        float f = 10.5f;
+        long l = 100000L;
+        boolean flag = true;
+        System.out.println(a);
+        System.out.println(f);
+        System.out.println(l);
+        System.out.println(flag);
+
+
+    }
+}
