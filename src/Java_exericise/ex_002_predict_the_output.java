1c3a2c03f6f5b9f121b93c50dc20288d67f4cbfc
Vishwajeet Vishwakarma <jeet31054@gmail.com>
2025-12-25 17:26:26 +0530

    adding execrise of predict the output.

---
+++ src/Java_exericise/ex_002_predict_the_output.java
@@ -0,0 +1,9 @@
+package Java_exericise;
+
+public class ex_002_predict_the_output {
+    static void main() {
+        int a = 10;
+        double b = 3;
+        System.out.println(a / b);  // (/) this is divide operators so, it gives 3.33333
+    }
+}
