718a2f1530c1c80b2df509bcb66832a9e400cf64
Vishwajeet Vishwakarma <jeet31054@gmail.com>
2025-12-25 17:27:21 +0530

    adding execrise type promation output.

---
+++ src/Java_exericise/ex_003_type_promotion.java
@@ -0,0 +1,10 @@
+package Java_exericise;
+
+public class ex_003_type_promotion {
+    static void main() {
+        char c = 'A';  // the value of a is 65 here
+        int x = c + 5;
+        System.out.println(x);
+//       the value of a is 65, after adding 5 in it gives output 70.
+    }
+}
