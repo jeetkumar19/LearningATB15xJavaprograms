6711d84d69d03183979ac631797042e5e0deeeec
Vishwajeet Vishwakarma <jeet31054@gmail.com>
2025-12-26 14:40:42 +0530

    adding_ a task of nested_ternery operator

---
+++ src/ex_06_ternary_operators/Lab0050_task.java
@@ -0,0 +1,12 @@
+package ex_06_ternary_operators;
+
+public class Lab0050_task {
+    static void main() {
+
+
+        int marks = 300;
+        String result = marks > 250 ? "pass" : "fail";
+        System.out.println(result);
+
+    }
+}
