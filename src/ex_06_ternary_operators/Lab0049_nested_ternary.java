a83a9f0be92e4baa191dea00e0b452e4e4a532ce
Vishwajeet Vishwakarma <jeet31054@gmail.com>
2025-12-26 14:40:15 +0530

    adding_nested_ternery operator

---
+++ src/ex_06_ternary_operators/Lab0049_nested_ternary.java
@@ -0,0 +1,9 @@
+package ex_06_ternary_operators;
+
+public class Lab0049_nested_ternary {
+    static void main() {
+        int jeet_age=21;
+        String result=jeet_age>18 ? (jeet_age>24?"he can drink" :"no"):"no you can not";
+        System.out.println(result);
+    }
+}
