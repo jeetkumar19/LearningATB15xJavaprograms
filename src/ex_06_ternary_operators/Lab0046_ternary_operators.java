7587beab89ec9efc51370ce0dad806fafb8844c1
Vishwajeet Vishwakarma <jeet31054@gmail.com>
2025-12-26 14:39:40 +0530

    adding_ternery operator

---
+++ src/ex_06_ternary_operators/Lab0046_ternary_operators.java
@@ -0,0 +1,10 @@
+package ex_06_ternary_operators;
+
+public class Lab0046_ternary_operators {
+    static void main() {
+        int i=-5;
+        String s=i>0 ? "+ve" : "-ve";
+        System.out.println(s);
+
+    }
+}
---
+++ src/ex_06_ternary_operators/Lab0047_extra_example.java
@@ -0,0 +1,9 @@
+package ex_06_ternary_operators;
+
+public class Lab0047_extra_example {
+    static void main() {
+        int a=10;
+        boolean b=a>5 ? true : false;
+        System.out.println(b);
+    }
+}
