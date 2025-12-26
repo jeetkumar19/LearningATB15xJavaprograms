e4918b31fbfd2f6762baa6846455d4ee9a5e84f0
Vishwajeet Vishwakarma <jeet31054@gmail.com>
2025-12-26 14:38:56 +0530

    adding_some extra type casting using "char"

---
+++ src/ex_05_type_casting/Lab0045_extra_example_char.java
@@ -0,0 +1,13 @@
+package ex_05_type_casting;
+
+public class Lab0045_extra_example_char {
+    static void main() {
+        char c='a';
+        int i=c;
+
+
+        int num=66;
+        char c1=(char)num;
+        System.out.println(c1);
+    }
+}
