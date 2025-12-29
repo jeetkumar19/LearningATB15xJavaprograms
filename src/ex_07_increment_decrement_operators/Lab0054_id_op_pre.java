3383e35e81ed4e0aafcf52a20fc591b3c827cc3b
Vishwajeet Vishwakarma <jeet31054@gmail.com>
2025-12-29 14:19:41 +0530

    adding_pre_incremental

---
+++ src/ex_07_increment_decrement_operators/Lab0054_id_op_pre.java
@@ -0,0 +1,11 @@
+package ex_07_increment_decrement_operators;
+
+public class Lab0054_id_op_pre {
+    static void main(String[] args) {
+
+        int a=10;
+        int b=++a;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
