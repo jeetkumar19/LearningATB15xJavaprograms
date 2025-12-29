372faa25b67114c50efe61ad8eb22e9843df1433
Vishwajeet Vishwakarma <jeet31054@gmail.com>
2025-12-29 14:20:08 +0530

    adding_post_incremental

---
+++ src/ex_07_increment_decrement_operators/Lab0055_post_increment.java
@@ -0,0 +1,10 @@
+package ex_07_increment_decrement_operators;
+
+public class Lab0055_post_increment {
+    static void main() {
+        int a=10;
+        int b=a++;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
